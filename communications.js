adacdecc81473048ada5a0a25ca9c0ac095ae2a0
Dan <andrei_dani_96@yahoo.com>
2014-03-02 19:30:52 +0200

    file request support and module request support

---
+++ communications.js
@@ -0,0 +1,58 @@
+var qs = require('querystring');
+var cached_modules = {};
+var link = {};
+
+exports.handleReq = function (req, module, operation, res) {
+
+    // cache module if it isn't already
+    if (!cached_modules[module]) {
+        cached_modules[module] = require('../modules/' + module + '/operations.js');
+    }
+
+    // build link
+    link = {
+        method: req.method,
+        headers: req.headers,
+        url: req.url,
+        res: res
+    }
+
+    // handle POST request
+    if (req.method === 'POST') {
+
+        //process POST data
+        processPost(req, function (err, data) {
+
+            if (err) {
+                res.writeHead(413, {'Content-Type': 'text/plain'}).end();
+                req.connection.destroy();
+                return;
+            }
+
+            // build link
+            link.data = data;
+
+            //call opperation
+            cached_modules[module][operation](link);
+        });
+    }
+}
+
+function processPost (req, callback) {
+    var queryData;
+
+    req.on('data', function(data) {
+        queryData += data;
+
+        // kill request if too large
+        if (queryData.length > 1e6) {
+            queryData = '';
+            return callback(new Error('Request data too large!'));
+        }
+    });
+
+    req.on('end', function() {
+        var body = qs.parse(queryData)
+        callback(null, body);
+    });
+}
--- server.js
+++ server.js
@@ -2,19 +2,65 @@ var http = require('http');
 var path = require('path');
 var fs = require('fs');
 var url = require('url');
+var config = require('../app.json');
+var comm = require('./communications.js');
 
 http.createServer(function (req, res) {
 
     var uri = url.parse(req.url).pathname;
-    var contentType = req.headers.accept;    
-    
-    console.log(contentType);
-    console.log(uri);
-    fs.readFile('index.html', 'binary', function (err, file) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(file, "binary");
-        res.end();
-    });
+    var contentType = req.headers.accept.split(',')[0];    
+    var method = req.method;
+
+    if (uri.substring(0, 3) !== '/@/' && method === 'GET') {
+        var filename;
+
+        // handle routes
+        if (contentType === 'text/html') {
+
+            if (config.routes[uri]) {
+                var page = config.routes[uri];
+                filename = '../' + config.pages[page].html;
+            } else {
+                filename = '../' + config.pages['not_found'].html;
+            }
+
+            fs.readFile(filename, "binary", function(err, file) {
+                if(err) {        
+                    res.writeHead(500, {"Content-Type": "text/plain"});
+                    res.write(err + "\n");
+                    res.end();
+                    return;
+                }
+
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(file, "binary");
+                res.end();
+            });
+        // handle rest
+        } else {
+            filename = '../public/' + uri;
+
+            fs.readFile(filename, "binary", function(err, file) {
+                if(err) {        
+                    res.writeHead(500, {"Content-Type": "text/plain"});
+                    res.write(err + "\n");
+                    res.end();
+                    return;
+                }
+
+                res.writeHead(200, {"Content-Type": contentType});
+                res.write(file, "binary");
+                res.end();
+            });
+        }
+    } else if (uri.substring(0, 3) === '/@/') {
+
+        var module = uri.split('/')[2];
+        var operation = uri.split('/')[3];
+
+        // send the req to the right module
+        comm.handleReq(req, module, operation, res);
+    }
 
 }).listen(7777);
 
