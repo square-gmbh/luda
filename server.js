429a7ad4799793fc4c0e99f564d9497cb780fd8b
Dan Andrei <andrei_dani_96@yahoo.com>
2014-10-18 20:02:28 +0300

    fixed public dir problems and handle routes using regex

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var app = express();
 var url = require('url');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var config = require('../app.json');
-var comm = require('./communications.js');
+var config = require(path.join(__dirname, '../app.json'));
+var comm = require('./lib/communications.js');
+var router = require('./lib/router.js');
 var port;
 
 // handle custom code
@@ -17,7 +18,7 @@ if (config.socket) {
 
 // configure app
 app.configure(function () {
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, '../public')));
     app.use(express.bodyParser());
     app.use(express.logger("short"));
     port = process.argv[2] || config.port;
@@ -34,7 +35,6 @@ app.get("*", function (req, res) {
 
     // get the request url
     var uri = url.parse(req.url).pathname;
-    var contentType = req.headers.accept.split(',')[0];
 
     // check if request is module or not
     if (uri.substring(0, 3) === '/@/') {
@@ -44,140 +44,8 @@ app.get("*", function (req, res) {
         res.writeHead(500);
         res.end('Not yet implemented');
     } else {
-
-        var filename;
-
         // handle routes
-        if (contentType === 'text/html') {
-
-            // check if page exists
-            uri = uri.match(/^\/.*?\/|^\/.*$/);
-            uri = uri[0].replace(/\/$/, "");
-            uri = uri || "/";
-            if (config.routes[uri]) {
-                var page = config.routes[uri];
-                filename = config.pages[page].html;
-                
-                // handle roles (if exist) (user permisions)
-                if (config.pages[page].roles) {
-
-                    // get role from session
-                    var roles = config.pages[page].roles;
-                    var role = (req.session.login) ? req.session.login.role : null;
-                    if (!role) role = 'visitator';
-
-                    // if the user doesn't have permission or role is missing run the fail action
-                    if (roles.indexOf(role) == -1) {
-
-                        // if no roleFail action or action is reject , reject user
-                        if (!config.pages[page].roleFail || config.pages[page].roleFail[0].reject) {
-                            res.writeHead(403);
-                            res.end("You don't have permission to view this page");
-                            return;
-                        }
-
-                        // get the action
-                        var action = config.pages[page].roleFail[0];
-
-                        if (action.redirect) {
-                            var location = 'http://' + req.headers.host + action.redirect;
-                            res.writeHead(302, {"location": location});
-                            res.end();
-                            return;
-                        }
-                    } else {
-                        // user has permision so he gets the file
-                        fs.readFile(filename, "binary", function(err, file) {
-
-                            if (err) {        
-                                res.writeHead(500, {"Content-Type": "text/plain"});
-                                res.write(err + "\n");
-                                res.end();
-                                return;
-                            }
-
-                            res.writeHead(200, {"Content-Type": "text/html"});
-                            res.write(file, "binary");
-                            res.end();
-                        });
-                    }
-
-                } else {
-                    // if no roles specified for current page give it as is
-                    fs.readFile(filename, "binary", function(err, file) {
-
-                        if (err) {        
-                            res.writeHead(500, {"Content-Type": "text/plain"});
-                            res.write(err + "\n");
-                            res.end();
-                            return;
-                        }
-
-                        res.writeHead(200, {"Content-Type": "text/html"});
-                        res.write(file, "binary");
-                        res.end();
-                    });
-                }
-            } else {
-
-                // check if the not found file exists
-                if (!config.pages['not_found']) {
-                    res.writeHead(404);
-                    res.write('404 Page not found');
-                    res.end();
-                    return;
-                }
-
-                // get the file
-                var filename = config.pages['not_found'].html;
-                // send the file
-                fs.readFile(filename, "binary", function(err, file) {
-
-                    if (err) {        
-                        res.writeHead(500, {"Content-Type": "text/plain"});
-                        res.write(err + "\n");
-                        res.end();
-                        return;
-                    }
-
-                    res.writeHead(200, {"Content-Type": "text/html"});
-                    res.write(file, "binary");
-                    res.end();
-                });
-            }
-        // handle scripts
-        } else if (contentType === "*/*") {
-            filename = 'public/' + uri;
-            
-            fs.readFile(filename, "binary", function(err, file) {
-                if (err) {
-                    res.writeHead(500, {"Content-Type": "text/plain"});
-                    res.write(err + "\n");
-                    res.end();
-                    return;
-                }
-
-                res.writeHead(200, {"Content-Type": "application/javascript"});
-                res.write(file, "binary");
-                res.end();
-            });
-        // handle rest
-        } else {
-            filename = 'public/' + uri;
-            
-            fs.readFile(filename, "binary", function(err, file) {
-                if (err) {
-                    res.writeHead(500, {"Content-Type": "text/plain"});
-                    res.write(err + "\n");
-                    res.end();
-                    return;
-                }
-
-                res.writeHead(200, {"Content-Type": contentType});
-                res.write(file, "binary");
-                res.end();
-            });
-        }
+        router.handleRoute(req, res);
     }
 });
 
